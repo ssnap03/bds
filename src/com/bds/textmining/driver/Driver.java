2312e94068fe7209fa421d4e2b3af8b4d3fd8fed
Anirudh Nistala <ani.nistala@gmail.com>
2023-03-20 17:25:24 -0400

    adds plotting for clusters

--- src/com/bds/textmining/driver/Driver.java
+++ src/com/bds/textmining/driver/Driver.java
@@ -1,24 +1,13 @@
-package com.bds.textmining.driver;
+package driver;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 import com.bds.textmining.preprocessing.*;
+import com.bds.textmining.algorithms.*;
+import com.bds.textmining.plotter.Plotter;
 
-import edu.stanford.nlp.pipeline.CoreDocument;
-import edu.stanford.nlp.pipeline.CoreEntityMention;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import com.bds.textmining.topicmodeling.TopicModeler;
-import com.bds.textmining.vectorization.Vectorizer;
+import vectorization.Vectorizer;
 
 public class Driver {
 
@@ -26,14 +15,68 @@ public class Driver {
 		Preprocessor preprocessor = new Preprocessor();
 		Vectorizer vectorizer = new Vectorizer();
 		List<List<String>> preprocessed = preprocessor.pipeline("src/data/", 3, 5);
-		System.out.println(preprocessed.size());
-		 Map<String, List<Double>> tfidf = (vectorizer.generateTfIdfMatrix(preprocessed));
-		 String[] l = vectorizer.getVocab(preprocessed);
+
+		Map<String, List<Double>> tfidf = (vectorizer.generateTfIdfMatrix(preprocessed));
+		 
+		 Map<Integer,Map<String,List<Double>>> clusters = Clustering.kmeansPlusPlus(tfidf, 3, "cosine");
+
+		 Map<String,List<Double> > c0 = clusters.get(0);
+		 Map<String,List<Double> > c1 = clusters.get(1);
+		 Map<String,List<Double> > c2 = clusters.get(2);
+		 
+		 List<String> docTitles0 = new ArrayList<String>(c0.keySet());
+		 List<String> docTitles1 = new ArrayList<String>(c1.keySet());
+		 List<String> docTitles2 = new ArrayList<String>(c2.keySet());
+		 
+		 List<List<Double> > f0 = new ArrayList<List<Double>>(c0.values());
+		 List<List<Double> > f1 = new ArrayList<List<Double>>(c1.values());
+		 List<List<Double> > f2 = new ArrayList<List<Double>>(c2.values());
+		 List<List<Double> > pcaIn = new ArrayList<List<Double>>(c0.values());
+		 
+		 System.out.println(f0.size());
+		 System.out.println(f1.size());
+		 System.out.println(f2.size());
+		 
+		 pcaIn.addAll(f1);
+		 pcaIn.addAll(f2);
+		
+		int[] clusterInfo = new int[pcaIn.size()];
+		String[] docTitles = new String[pcaIn.size()];
+		
+		int k = 0;
+		for(int j =0; j<f0.size(); j++) {
+			clusterInfo[k] = 0;
+			docTitles[k++] = docTitles0.get(j);
+		}
+		for(int j =0; j<f1.size(); j++) {
+			clusterInfo[k] = 1;
+			docTitles[k++] = docTitles1.get(j);
+		}
+		for(int j =0; j<f2.size(); j++) {
+			clusterInfo[k] = 2;
+			docTitles[k++] = docTitles2.get(j);
+		}
+		
+		double[][] out = DimensionalityReduction._PCA(pcaIn,2);
+		 
+		for(int i = 0; i<out.length; i++) {
+			System.out.print("(");
+			for(int j = 0; j < out[0].length; j++) {
+				System.out.print(out[i][j]);
+				System.out.print(",");
+			}
+			System.out.println(")");
+		}
+		
+		Plotter.plotClusters(out, clusterInfo, docTitles);
+		 
+//		 String[] l = vectorizer.getVocab(preprocessed);
 		 
 		
-	     TopicModeler t = new TopicModeler();
-	     t.extractTopics(tfidf, Arrays.asList(vectorizer.getVocab(preprocessed)), 8);
+//	     TopicModeler t = new TopicModeler();
+//	     t.extractTopics(tfidf, Arrays.asList(vectorizer.getVocab(preprocessed)), 8);
 	
 		
 	}
+
 }
--- src/com/bds/textmining/plotter/Plotter.java
+++ src/com/bds/textmining/plotter/Plotter.java
@@ -1,43 +1,83 @@
 package com.bds.textmining.plotter;
 
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.chart.JFreeChart;
+//import org.jfree.chart.labels.StandardXYItemLabelGenerator;
+//import org.jfree.chart.labels.XYItemLabelGenerator;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import javax.swing.JFrame;
+
+import java.awt.Color;
 import java.util.List;
-import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.ChartPanel;
 
 public class Plotter {
 	public static void plot2d(double[][] input, List<String> labels) {
 	    
-	    JFrame f = new JFrame("JDBCTest");
+	    JFrame f = new JFrame("Documents");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       
         
         JFreeChart chart = ChartFactory.createScatterPlot(
 	            "Scatter Plot", 
-	            "X", 
-	            "Y", 
+	            "tfidf", 
+	            "docName", 
 	            createDataset(input),
 	            PlotOrientation.VERTICAL,
 	            true,
 	            true,
 	            false
 	            );
+        
         XYPlot plot = chart.getXYPlot();
-        plot.setDomainAxis(new DateAxis("Date"));
         f.add(new ChartPanel(chart));
         f.pack();
         f.setLocationRelativeTo(null);
         f.setVisible(true);
         
 		
+	}
+	
+	public static void plotClusters(double[][] input, int[] clusterInfo, String[] docTitles) {
 		
+		XYSeriesCollection dataset = new XYSeriesCollection();  
+        
+		int k = 3;
+		
+        for (int i = 0; i < k; i++) {
+            XYSeries series = new XYSeries("Cluster " + (i + 1));
+            for (int j = 0; j < clusterInfo.length; j++) {
+                if (clusterInfo[j] == i) {
+                    series.add(input[j][0], input[j][1]);
+                }
+            }
+            dataset.addSeries(series);
+        }
+
+        JFreeChart chart = ChartFactory.createScatterPlot(
+        		"Clusters Plot", 
+        		"tfidf", 
+        		"document", 
+        		dataset, 
+        		PlotOrientation.VERTICAL, 
+        		true, 
+        		true, 
+        		false);
+
+        int[] colors = {Color.GREEN.getRGB(), Color.YELLOW.getRGB(), Color.RED.getRGB(), Color.BLUE.getRGB(), Color.ORANGE.getRGB()};
+        int i = 0;
+        for (Object seriesObj : dataset.getSeries()) {
+            chart.getXYPlot().getRenderer().setSeriesPaint(i++, new Color(colors[i%colors.length]));
+        }
+
+        ChartFrame frame = new ChartFrame("Cluster Plot", chart);
+        frame.pack();
+        frame.setVisible(true);
 	}
 	
 	private static XYDataset createDataset(double[][] input) {
